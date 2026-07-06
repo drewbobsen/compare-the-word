bb7e9ad645d821aa71affb0e98cbd22e963352d7
Andrew Erhardt <andrew@erhardt.cloud>
2026-07-06 04:39:11 -0700

    First time didn't fix it, let's try this again.....

--- frontend/app/sitemap.ts
+++ frontend/app/sitemap.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = 'force-dynamic';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://api:8080";
 
